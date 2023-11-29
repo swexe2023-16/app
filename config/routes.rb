10fb00b445387c3a8611b1fc2abe3fdd54446c9c
茅野恭平 <2111140015s@ed.fuk.kindai.ac.jp>
2023-11-29 06:58:05 +0000

    ヘッダー作成1

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   get 'posts/index'
   get '/' => "home#top"
   get "/about" => "home#about"
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
